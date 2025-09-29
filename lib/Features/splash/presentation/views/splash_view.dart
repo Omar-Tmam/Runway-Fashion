0a6ff806af47ec5852d09787dd5df73f87e38fa7
omartmam <omartmam9@gmail.com>
2025-09-29 18:10:43 +0300

    finish splash

--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:runway_app/Core/utils/assets.dart';
+import 'package:runway_app/Features/splash/presentation/views/widgets/line_animation.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Future.delayed(Duration(milliseconds: 800), () {
+    
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              Assets.imagesRunway,
+              width: 188,
+            ),
+            Gap(6),
+            LineAnimation(
+                child: SvgPicture.asset(
+              Assets.imagesRectangle,
+              width: 189,
+            ))
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/Features/splash/presentation/views/widgets/line_animation.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LineAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  const LineAnimation({super.key, required this.child, this.duration = const Duration(milliseconds: 800)});
+
+  @override
+  State<LineAnimation> createState() => _LineAnimationState();
+}
+
+class _LineAnimationState extends State<LineAnimation> with SingleTickerProviderStateMixin {
+  late final AnimationController _ctrl = AnimationController(vsync: this, duration: widget.duration)
+    ..forward();
+  late final Animation<double> _anim = CurvedAnimation(parent: _ctrl, curve: Curves.easeOut);
+
+  @override
+  void dispose() {
+    _ctrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _anim,
+      builder: (_, __) {
+        final lead = (_anim.value - 0.1).clamp(0.0, 1.0);
+        final tail = _anim.value.clamp(0.0, 1.0);
+
+        return ShaderMask(
+          blendMode: BlendMode.dstIn,
+          shaderCallback: (Rect bounds) {
+            return LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: const [Colors.black, Colors.transparent],
+              stops: [tail, lead],
+            ).createShader(bounds);
+          },
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
