b4f79a8f8419e91b1190c9002de07e4ac9c17f6f
omartmam <omartmam9@gmail.com>
2025-09-29 17:20:59 +0300

    fix gradle errors

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -2,7 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:runway_app/Features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
-  static const kSplash = 'splash';
+  static const kSplash = '/';
   static final router = GoRouter(routes: [
     GoRoute(
       path: kSplash,
