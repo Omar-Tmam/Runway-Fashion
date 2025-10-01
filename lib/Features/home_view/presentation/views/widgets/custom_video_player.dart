4048d1e637457b8e04b350053174108f2cafcdbe
omartmam <omartmam9@gmail.com>
2025-10-01 21:16:49 +0300

    test

--- lib/Features/home_view/presentation/views/widgets/custom_video_player.dart
+++ lib/Features/home_view/presentation/views/widgets/custom_video_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:runway_app/Core/utils/assets.dart';
-import 'package:video_player/video_player.dart';
+import 'package:media_kit/media_kit.dart';
+import 'package:media_kit_video/media_kit_video.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   const CustomVideoPlayer({super.key});
@@ -10,21 +11,31 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
-  late VideoPlayerController _controller;
+  late final Player _player;
+  late final VideoController _controller;
+
   @override
   void initState() {
-    _controller = VideoPlayerController.asset(Assets.imagesVideo)
-      ..initialize().then((_) {
-        setState(() {
-          _controller.play();
-        });
-      })
-      ..setLooping(true);
     super.initState();
+    _player = Player();
+    _controller = VideoController(_player);
+    
+    _player.open(Media('asset:///${Assets.imagesVideo}'));
+    _player.setPlaylistMode(PlaylistMode.loop);
+    _player.play();
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(aspectRatio: _controller.value.aspectRatio, child: VideoPlayer(_controller));
+    return Video(
+      controller: _controller,
+      fit: BoxFit.contain,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:media_kit/media_kit.dart';
 import 'package:runway_app/Core/utils/app_router.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MediaKit.ensureInitialized();
   runApp(const RunWayFashion());
 }
 
@@ -11,6 +14,8 @@ class RunWayFashion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark().copyWith(),
       theme: ThemeData.light().copyWith(
           appBarTheme: AppBarTheme(
               surfaceTintColor: Colors.transparent, backgroundColor: Colors.transparent),
